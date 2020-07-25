f2540b3c8188b66572cf6cf52d88edc2c678400a
Ruiz Sanchez <ruizcayetano@hotmail.com>
2020-07-25 17:37:34 -0600

    added README.md file

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Good-README-Generator
+
+    #ME
+    <h5>Discription:</h5>
+    <p class="lead"> 1.</p>
+    <h3><span class="badge badge-secondary">Table of Contents</span></h3>
+    <ul class="list-group">
+      <li class="list-group-item">My GitHub username is 1</li>
+      <li class="list-group-item">LinkedIn: 1</li>
+ >
--- index.js
+++ index.js
@@ -41,39 +41,24 @@ function promptUser() {
 
 function generateHTML(answers) {
     return `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">${answers.name}</h1>
+    #${answers.name}
     <h5>Discription:</h5>
     <p class="lead"> ${answers.description}.</p>
     <h3><span class="badge badge-secondary">Table of Contents</span></h3>
     <ul class="list-group">
       <li class="list-group-item">My GitHub username is ${answers.github}</li>
       <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>`;
+ >`;
 }
 
 promptUser()
     .then(function (answers) {
-        const html = generateHTML(answers);
+        const md = generateHTML(answers);
 
-        return writeFileAsync("index.html", html);
+        return writeFileAsync("README.md", md);
     })
     .then(function () {
-        console.log("Successfully wrote to index.html");
+        console.log("Successfully wrote to README.md");
     })
     .catch(function (err) {
         console.log(err);
